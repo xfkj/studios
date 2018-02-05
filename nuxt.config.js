7d00f0ea94d0e1d0c7d5b9ce8c47118962ebeb53
xfkj <it@ffjy.org>
2018-02-05 20:26:52 +0800

    修改首页标题，desc

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,12 +3,12 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: '杭州画室排名,杭州美术培训班，老鹰画室，孪生画室，白塔岭画 室，厚一画室-非凡教育官方网站',
+    title: '杭州画室排名,杭州美术培训班，老鹰画室，孪生画室，白塔岭画室，厚一画室-非凡教育官方网站',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'keywords', name: 'keywords', content: ' 杭州画室排名,杭州美术培训班，老鹰 画室，孪生画室，白塔岭画室，厚一画室' },
-      { hid: 'description', name: 'description', content: ' 非凡教育一家知名的艺术生文化课 补习培训机构，与杭州各大画室美术培训中心，都建立的良好的合作关系 ，长期为老鹰，孪生，厚一，白塔岭等提供高考冲刺提升课程。' }
+      { hid: 'description', name: 'description', content: '非凡教育一家知名的艺术生文化课 补习培训机构，与杭州各大画室美术培训中心，都建立的良好的合作关系 ，长期为老鹰画室，孪生画室，厚一画室，白塔岭画室等提供高考冲刺提升课程。' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
